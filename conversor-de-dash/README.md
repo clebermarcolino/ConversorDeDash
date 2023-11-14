061a706db4062c3aa13315cae530d3aabc4c215d
clebermarcolino <cleber.marcolino@dcx.ufpb.br>
2023-11-14 13:38:48 -0300

    First commit

---
+++ conversor-de-dash/README.md
@@ -0,0 +1,4 @@
+# Conversor de Dash
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Cleber-Marcolino/pen/abXyNGb](https://codepen.io/Cleber-Marcolino/pen/abXyNGb).
+
---
+++ conversor-de-dash/src/script.js
@@ -0,0 +1,8 @@
+var valorEmDash = 60;
+var cotacaoDoDash = 156.51;
+var nome = "Marcos";
+
+var valorEmReal = valorEmDash * cotacaoDoDash;
+valorEmReal = valorEmReal.toFixed(2);
+
+alert("Olá, " + nome + "! o valor em reais é R$ " + valorEmReal);
